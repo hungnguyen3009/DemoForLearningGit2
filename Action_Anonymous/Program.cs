5409b1822e3af9b4ecc878cf4994db253c03c820
Nguyen Xuan Hung <nguyenxuanhung3009@outlook.com>
2020-03-01 10:02:51 +0700

    First Commit Example

---
+++ Action_Anonymous/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Action_Anonymous
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
